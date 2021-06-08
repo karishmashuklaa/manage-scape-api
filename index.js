ff9b4a47df7291ad8fb76b0dac2221cb1111c933
karishmas6 <carishmashukla28@gmail.com>
2021-06-08 19:54:33 +0530

    Post endpoints

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const pathToFile = path.resolve('./data.json')
 
 const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
 
+app.use(express.json()) // to console.log the req.body
+
 app.get('/', (req,res) => {
     res.send('Hello world')
 })
@@ -16,6 +18,12 @@ app.get('/api/resources', (req, res) => {
     const resources = getResources()
     res.send(resources)
   })
+
+app.post('/api/resources' , (req,res) => {
+    const resources = getResources()
+    console.log(req.body)
+    res.send('Your response has been recieved successfully')
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
